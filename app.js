bae2741ac404c74c137f5f6359e73bdc3ef6d7c6
Rahul <joshi.rj21@gmail.com>
2020-01-18 17:48:43 +0530

    hero section

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 // Animation Toggler
 
-// var toggler = document.querySelector(".toggler");
-// var btn_toggler = document.querySelector(".nav-links-toggler");
+var toggler = document.querySelector(".toggler");
+var btn_toggler = document.querySelector(".nav-links-toggler");
 
-// var togglerFlag = false;
-// var running = false;
+var togglerFlag = false;
+var running = false;
 
-// btn_toggler.addEventListener("click",(ev)=>{
-//     ev.preventDefault();
-//     if(!togglerFlag && !running){
-//         anime({
-//             targets: '.toggler',
-//             direction: 'normal',
-//             easing: 'linear',
-//             width: ['0%', '30%'],
-//             begin: function () {
-//                 running = true;
-//             },
-//             complete: function () {
-//                 togglerFlag = true;
-//                 running = false;
-//             }
-//         });
-//     }
-//     else if(togglerFlag && !running){
-//         anime({
-//             targets: '.toggler',
-//             direction: 'normal',
-//             easing: 'linear',
-//             width: ['30%', '0%'],
-//             begin: function () {
-//                 running = true;
-//             },
-//             complete: function () {
-//                 togglerFlag = false;
-//                 running = false;
-//             }
-//         });
-//     }
-// })
+btn_toggler.addEventListener("click",(ev)=>{
+    ev.preventDefault();
+    if(!togglerFlag && !running){
+        anime({
+            targets: '.toggler',
+            direction: 'normal',
+            easing: 'linear',
+            width: ['0%', '30%'],
+            begin: function () {
+                running = true;
+            },
+            complete: function () {
+                togglerFlag = true;
+                running = false;
+            }
+        });
+    }
+    else if(togglerFlag && !running){
+        anime({
+            targets: '.toggler',
+            direction: 'normal',
+            easing: 'linear',
+            width: ['30%', '0%'],
+            begin: function () {
+                running = true;
+            },
+            complete: function () {
+                togglerFlag = false;
+                running = false;
+            }
+        });
+    }
+})
 
 
